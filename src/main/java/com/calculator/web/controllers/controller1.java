fd2dfe3a90b53604ad075b04d9242ccc38ed6b57
Ivan_kush <ivan.m.kush@gmail.com>
2022-05-09 15:22:43 +0400

    work 3 pages with blocks too

--- src/main/java/com/calculator/web/controllers/controller1.java
+++ src/main/java/com/calculator/web/controllers/controller1.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class controller1 {
     @GetMapping("/FAQs")
     public String FAQsMain(Model model) {
+        model.addAttribute("title", "FAQs");
         return "FAQs";
     }
 }
