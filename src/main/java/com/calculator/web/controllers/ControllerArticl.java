18cc528a41dd916e093df274c4fd99346473b0d6
Ivan_kush <ivan.m.kush@gmail.com>
2022-05-14 19:27:37 +0400

    news page fail

--- src/main/java/com/calculator/web/controllers/ControllerArticl.java
+++ src/main/java/com/calculator/web/controllers/ControllerArticl.java
@@ -6,8 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.calculator.web.repo.PostRepository;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class ControllerArticl {
@@ -23,14 +27,25 @@ public class ControllerArticl {
         }
 
     @GetMapping("/Articles/add")
-    public String Articleadd(Model model) {
+    public String Articleaddd(Model model) {
         model.addAttribute("articles", "Articles-add");
         return "Articles-add";
     }
     @PostMapping("/Articles/add")
     public String Articleaddpost(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model){
-           Article article = new Article(title, anons, full_text);
-           articleRepository.save(article);
+           Article articlee = new Article(title, anons, full_text);
+           articleRepository.save(articlee);
            return "redirect:/Articles";
+    }
+    @GetMapping("/Articles/{id}")
+    public String ArticleDetails(@PathVariable(value = "id") long id, Model model) {
+           if(!articleRepository.existsById(id)) {
+               return "redirect:/Articles";
+           }
+        Optional<Article> articlee = articleRepository.findById(id);
+        ArrayList<Article> art = new ArrayList<>();
+        articlee.ifPresent(art::add);
+        model.addAttribute("articlee", art);
+        return "Articles-details";
     }
 }
